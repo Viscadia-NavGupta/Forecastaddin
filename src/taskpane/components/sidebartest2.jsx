262b5df23e83c064f60c52196d5e40b9ccf1ee18
Viscadia-NavGupta <navendu.gupta@viscadia.com>
2024-07-08 11:40:03 +0530

    Updated UI

--- src/taskpane/components/sidebartest2.jsx
+++ src/taskpane/components/sidebartest2.jsx
@@ -1,30 +1,24 @@
-import React, { useState, useRef } from 'react';
-import 'boxicons/css/boxicons.min.css';
-import {
-  Container,
-  Sidebar,
-  MenuSection,
-  MenuItem,
-  BottomSection,
-} from './sidebartest2styles';
-import Tooltip from './Tooltip';
+import React, { useState, useRef } from "react";
+import "boxicons/css/boxicons.min.css";
+import { Container, Sidebar, MenuSection, MenuItem, BottomSection } from "./sidebartest2styles";
+import Tooltip from "./tooltip";
 
 const Sidebartest2 = ({ onMenuItemClick, handleLogout }) => {
-  const [tooltipText, setTooltipText] = useState('');
+  const [tooltipText, setTooltipText] = useState("");
   const [tooltipVisible, setTooltipVisible] = useState(false);
   const tooltipRef = useRef(null);
 
   const menuItems = [
-    { icon: 'bx bx-grid-alt', text: 'Model Management', onClick: () => onMenuItemClick('ModelManagementPage1') },
-    { icon: 'bx bx-collection', text: 'Model Editor', onClick: () => onMenuItemClick('Model Editor') },
-    { icon: 'bx bx-cog', text: 'Scenario management', onClick: () => onMenuItemClick('Scenario management') },
-    { icon: 'bx bx-user', text: 'Risk Analytics', onClick: () => onMenuItemClick('Risk Analytics') },
-    { icon: 'bx bx-calendar', text: 'Load Scenario', onClick: () => onMenuItemClick('LoadPage') },
-    { icon: 'bx bx-message', text: 'TBD', onClick: () => onMenuItemClick('ModelManagementPage') },
-    { icon: 'bx bx-bell', text: 'TBD', onClick: () => onMenuItemClick('ModelManagementPage') },
-    { icon: 'bx bx-book', text: 'TBD', onClick: () => onMenuItemClick('ModelManagementPage') },
-    { icon: 'bx bx-photo-album', text: 'TBD', onClick: () => onMenuItemClick('ModelManagementPage') },
-    { icon: 'bx bx-help-circle', text: 'Help', onClick: () => onMenuItemClick('ModelManagementPage') },
+    { icon: "bx bx-grid-alt", text: "Model Management", onClick: () => onMenuItemClick("ModelManagementPage1") },
+    { icon: "bx bx-collection", text: "Model Editor", onClick: () => onMenuItemClick("Model Editor") },
+    { icon: "bx bx-cog", text: "Scenario management", onClick: () => onMenuItemClick("Scenario management") },
+    { icon: "bx bx-user", text: "Risk Analytics", onClick: () => onMenuItemClick("Risk Analytics") },
+    { icon: "bx bx-calendar", text: "Load Scenario", onClick: () => onMenuItemClick("LoadPage") },
+    { icon: "bx bx-message", text: "TBD", onClick: () => onMenuItemClick("ModelManagementPage") },
+    { icon: "bx bx-bell", text: "TBD", onClick: () => onMenuItemClick("ModelManagementPage") },
+    { icon: "bx bx-book", text: "TBD", onClick: () => onMenuItemClick("ModelManagementPage") },
+    { icon: "bx bx-photo-album", text: "TBD", onClick: () => onMenuItemClick("ModelManagementPage") },
+    { icon: "bx bx-help-circle", text: "Help", onClick: () => onMenuItemClick("ModelManagementPage") },
   ];
 
   const handleMouseEnter = (text, ref) => {
@@ -56,10 +50,10 @@ const Sidebartest2 = ({ onMenuItemClick, handleLogout }) => {
         <BottomSection>
           <MenuItem
             onClick={handleLogout}
-            onMouseEnter={(e) => handleMouseEnter('Logout', e.currentTarget)}
+            onMouseEnter={(e) => handleMouseEnter("Logout", e.currentTarget)}
             onMouseLeave={handleMouseLeave}
           >
-            <i className='bx bx-log-out'></i>
+            <i className="bx bx-log-out"></i>
           </MenuItem>
         </BottomSection>
       </Sidebar>
